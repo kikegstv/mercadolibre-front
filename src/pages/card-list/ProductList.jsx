f75d4cd1a650fc43779e5c764a30314ac2b8a614
kikegstv <garciastave.enrique@gmail.com>
2023-03-10 09:51:42 -0500

    Breadcrumb

--- src/pages/card-list/ProductList.jsx
+++ src/pages/card-list/ProductList.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { ProductContext } from "../../context/ProductContext";
+import Breadcrumb from "../../shared/breadcrumb/Breadcrumb";
 import Card from "../../shared/card/Card";
 import NavBar from "../../shared/navbar/NavBar";
 import "./ProductList.css";
@@ -17,19 +18,20 @@ function ProductList() {
   useEffect(() => {
     fetch(`https://mercadolibre-api.onrender.com/api/items?q=${searchText}`)
       .then((response) => response.json())
-      .then((data) => getProducts(data.body.items));
+      .then((data) => getProducts(data.body));
   }, [searchText]);
 
   if (products.length === 0) {
     return <h1>Cargando...</h1>;
   }
-
+  console.log(products);
   return (
     <>
       <div>
         <main className="product-list">
           <section className="product-list__container">
-            {products.map((item) => (
+            <Breadcrumb items={products.categories}></Breadcrumb>
+            {products.items.map((item) => (
               <Card key={item.id} item={item}></Card>
             ))}
           </section>
---
+++ src/shared/breadcrumb/Breadcrumb.jsx
@@ -0,0 +1,18 @@
+import "./Breadcrumb.css";
+import arrow from "./../../assets/icons/arrow.svg";
+
+function Breadcrumb({ items }) {
+  console.log(items);
+  return (
+    <>
+      <section className="breadcrumb">
+        {items.map((item, key) => (
+          <div className="breadcrumb__container">
+            <p key={key}>{item}</p> {key < (items.length - 1) && (<img className="breadcrumb__container__image" src={arrow} alt="breadcrumb icon"></img>)}
+          </div>
+        ))}
+      </section>
+    </>
+  );
+}
+export default Breadcrumb;
